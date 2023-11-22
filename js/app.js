aa60205715f7b020fa1bab68ab2ad31763f965e0
Clazariuk1 <Clazariuk@gmail.com>
2023-11-21 19:11:03 -0800

    Chapter 4 complete - Made the Baddies

--- js/app.js
+++ js/app.js
@@ -117,13 +117,17 @@ function keepItSecretKeepItSafe() {
 
 function makeBaddies() {
   console.log("4: makeBaddies");
-
+  const baddiesContainer = document.createElement('ul');
   // display an unordered list of baddies in Mordor
-
+  for(let baddy of baddies) {
+    const baddiesLI = document.createElement('li');
+    baddiesLI.classList.add('baddy');
+    baddiesLI.innerText = baddy;
+    baddiesContainer.appendChild(baddiesLI);
+  }
   // give each of the baddies a class of "baddy"
-
+  document.getElementById('mordor').appendChild(baddiesContainer)
   // remember to append them to Mordor
-
 }
 
 // COMMIT YOUR WORK
